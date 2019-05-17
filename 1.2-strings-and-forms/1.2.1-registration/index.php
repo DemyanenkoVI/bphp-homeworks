739ee3681269142470c92a5fc5d4ccc0c08763f7
DemyanenkoVI <vista_jjet@mail.ru>
2019-05-18 02:18:01 +0300

    first commit

---
+++ 1.2-strings-and-forms/1.2.1-registration/index.php
@@ -0,0 +1,58 @@
+<?php
+    $login = htmlspecialchars($_POST['login']); 
+    $password = htmlspecialchars($_POST['password']);
+    $email = htmlspecialchars($_POST['email']);
+    $firstName = htmlspecialchars($_POST['firstName'];
+    $lastName = htmlspecialchars($_POST['lastName'];
+    $middleName = htmlspecialchars($_POST['middleName'];
+    $code = htmlspecialchars($_POST['code'];
+    $array = array( "@","/","*","?",",",";",":");
+    $mask = '/[a-z0-9]*@[a-z0-9]*.[a-z0-9]*/';
+    $codeWord = 'qwerty'
+    //$login = 'Alexandr'; 
+    //$password = 'l1j9v';
+    //$email = 'somemail@email.com';
+    //$firstName = 'Иван';
+    //$lastName = 'Иванов';
+    //$middleName = 'Иванович';
+    //$code = 'nd82jaake';
+
+    $array = array( "@","/","*","?",",",";",":");
+    $mask = '/[a-z0-9]*@[a-z0-9]*/.[a-z0-9]*/';
+
+
+function Err($string1, $arr) {
+    $a=0;
+    for ($i=0; $i < strlen($string1); $i++) {
+        if (in_array($string1[$i], $arr)) {
+            $a = 1;
+            break;    
+        } else {
+            $a = 0;
+        }
+}
+return $a;
+}
+ 
+if (Err($login,$array) == 1) {
+    $string = "Поле логин не должно содержать символы @/*?,;:";
+}
+if (strlen($password) <= 8) {
+    $string = $string.PHP_EOL."Длина пароля должна быть минимум 8 символов ";
+}
+if (preg_match($mask,$email) == false) {
+    $string = $string.PHP_EOL."Неверный формат email";
+}
+if (strlen($firstName) == 0) {
+    $string = $string.PHP_EOL."Имя обязательно к заполнению";
+}
+if (strlen($lastName) == 0) {
+    $string = $string.PHP_EOL."Фамилия обязательна к заполнению";
+}
+if (strlen($middleName) == 0) {
+    $string = $string.PHP_EOL."Отчество обязательно к заполнению";
+}    
+echo $string;        
+}
+
+?>
