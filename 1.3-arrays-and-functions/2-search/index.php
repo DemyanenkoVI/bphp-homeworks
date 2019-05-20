641ac90489339fb391b7cd4d52a02ef898b8159e
DemyanenkoVI <vista_jjet@mail.ru>
2019-05-20 23:46:38 +0300

    first commit

---
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -0,0 +1,44 @@
+<?php
+function search($quantity, $map) {
+    $row = count($map);
+    $column = count($map[0]);
+for ($i=0; $i < $row; $i++) {
+    $place = 0;
+    for ($j=0; $j < $column; $j++) {
+        if ($j > $column - $quantity + 1) {
+            continue;
+        }
+        if ($map[$i][$j] == 0) {
+            $place = $place + 1;
+            if ($place == $quantity) {
+                echo 'Возможные места ряд: '. ($i+1) . ' места с: ' . ($j+1 - $quantity+1). ' по: ' . ($j+1) . '<br></br>';
+                continue;
+            }
+        } else
+            $place = 0;
+        
+    }
+}
+}
+function generate($rows, $placesPerRow, $avaliableCount) {
+    if ($rows * $placesPerRow  > $avaliableCount) {
+        $result = FALSE;
+    } else {
+        $result = array(array());
+        for ($i = 0; $i <= $rows-1; $i++) {
+            for ($j = 0; $j <= $placesPerRow-1; $j++) {
+                $result[$i][$j] = rand(0,1);
+                //echo $i;
+            }
+        }
+    }
+
+return $result;
+}
+
+$map = generate(5,10,51);
+
+search(3,$map);
+
+
+?>   
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bPHP</title>
+</head>
+<body>
+<ul>
+    <li>Блок 1. Базовый синтаксис. PHP в HTML и HTML средствами PHP
+        <ul>
+            <li>Занятие 1.1. Знакомство с PHP
+                <ul>
+                    <li><a href="bphp-homeworks/1.1-intro-and-branching/1.1.1">Задание 1.1.1</a></li>
+                    <li><a href="bphp-homeworks/1.1-intro-and-branching/1.1.2">Задание 1.1.2</a></li>
+                </ul>
+            </li>
+            <li>Занятие 1.2. Формы и строки
+                <ul>
+                    <li><a href="bphp-homeworks/1.2-strings-and-forms/1.2.1-registration/form.html">Задание 1.2.1</a></li>
+                    <li><a href="bphp-homeworks/1.2-strings-and-forms/1.2.2-name-standart/form.html">Задание 1.2.2</a></li>
+                    <li><a href="bphp-homeworks/1.2-strings-and-forms/1.2.3-additional/form.html">Задание 1.2.3</a></li>
+                </ul>
+            </li>
+            <li>Занятие 1.1. Массивы и циклы
+                <ul>
+                    <li><a href="bphp-homeworks/1.3-arrays-and-functions/1-places">Задание 1.3.1</a></li>
+                    <li><a href="bphp-homeworks/1.3-arrays-and-functions/2-search">Задание 1.3.2</a></li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+</ul>
+</body>
+</html>
+
